48ae20d248d5efaf2db0162018504c0cef603dc7
Vijay Kumavat <kumavatvijay776@gmail.com>
2023-04-15 18:39:09 +0530

    form handling

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-edit',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostEditComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null,
+        Validators.required),
+      description: new FormControl(null,
+        Validators.required),
+      imagePath: new FormControl(null,
+        Validators.required)
+    })
   }
 
+  OnSubmit(){
+    const title = this.form.value.title;
+    const description = this.form.value.description;
+    const imagePath = this.form.value.imagePath;
+
+    // ready with the object
+    const post: Post = new Post(
+      title,
+      description,
+      imagePath,
+      "test@test.com",
+      new Date()
+    )
+
+    this.postService.addPost(post);
+
+    this.router.navigate(['/post-list']);
+  }
 }
